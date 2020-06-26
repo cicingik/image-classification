509350e667e9686519e488db131f3374a6a056d3
tiara <mutiara.fatwa@mamiteam.com>
2020-06-27 00:48:35 +0700

    add nasnetL

--- cnf/config.py
+++ cnf/config.py
@@ -3,7 +3,7 @@ import os
 
 # Basic config for foldering
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = 'C:/Users/Gilang/Documents/Sopikodelig/Image_Recognition/'
+DATA_DIR = 'content/'
 TRAIN_DIR = os.path.join(DATA_DIR, 'train/train/')
 TEST_DIR = os.path.join(DATA_DIR, 'test/test/')
 TEST_FILE = os.path.join(DATA_DIR, 'test.csv')
@@ -16,7 +16,7 @@ MODEL = os.path.join(MODEL_DIR, 'version-1.h5')
 # Config for training
 IMAGE_SIZE = 224        # Image size for input training
 IMAGE_CHANNEL = 3       # Image channel
-BATCH_SIZE = 100         # How much image will train in a batch
+BATCH_SIZE = 124         # How much image will train in a batch
 EPOCH_NUM = 10          # Total epoch for training
 TEST_SIZE = 0.20        # Composition train and test data
 LEARNING_RATE = 1e-4    # Learning rate
--- models/models.py
+++ models/models.py
@@ -5,6 +5,7 @@ import colorama
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten, Dense, Dropout
 from tensorflow.keras import optimizers
+from tensorflow.keras.applications import NASNetLarge
 from tensorflow.keras.applications.vgg16 import VGG16
 from cnf.config import (IMAGE_SIZE, IMAGE_CHANNEL, BATCH_SIZE, EPOCH_NUM,
                         MODEL_DIR, TRAIN_VERSION, LEARNING_RATE, DECAY, CLASS_NUM)
@@ -32,9 +33,25 @@ class Models:
 
         return top_model
 
+    @property
+    def __build_nasnetL(self):
+        model = NASNetLarge(weights='imagenet', include_top=False, input_shape=(IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNEL))
+        model.trainable = False
+        
+        top_model = Sequential()
+        top_model.add(model)
+        top_model.add(Flatten())
+        top_model.add(Dense(256, activation='relu'))
+        top_model.add(Dropout(0.3))
+        top_model.add(Dense(CLASS_NUM, activation='sigmoid'))
+
+        return top_model
+
     def build_model(self):
         if self.model_type == 'vgg16':
             model = self.__build_vgg16
+        elif self.model_type == 'nasnetL':
+            model = self.__build_nasnetL
         else:
             raise Exception('Invalid model name')
 
@@ -46,10 +63,11 @@ class Models:
             loss='sparse_categorical_crossentropy',
             optimizer=optimizers.Adam(lr=LEARNING_RATE, decay=DECAY),
             metrics=['accuracy'])
-        self.model.fit_generator(
-            self.train_set,
+        self.model.fit(
+            x = self.train_set,
             steps_per_epoch=self.train_set.n // BATCH_SIZE,
             epochs=EPOCH_NUM,
+            workers=6,
             validation_data=self.validation_set,
             validation_steps=self.validation_set.n // BATCH_SIZE)
 
