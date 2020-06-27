b481fe8a4745dfac88c812aa74472b10addfdedb
tiara <mutiara.fatwa@mamiteam.com>
2020-06-28 00:08:27 +0700

    add inceptionresnetv2

--- models/models.py
+++ models/models.py
@@ -3,10 +3,10 @@ import os
 import sys
 import colorama
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Flatten, Dense, Dropout
+from tensorflow.keras.layers import Flatten, Dense, Dropout, GlobalAveragePooling2D
 from tensorflow.keras import optimizers
 from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
-from tensorflow.keras.applications import NASNetLarge, Xception
+from tensorflow.keras.applications import NASNetLarge, Xception, InceptionResNetV2
 from tensorflow.keras.applications.vgg16 import VGG16
 from cnf.config import (IMAGE_SIZE, IMAGE_CHANNEL, BATCH_SIZE, EPOCH_NUM,
                         MODEL_DIR, TRAIN_VERSION, LEARNING_RATE, DECAY, CLASS_NUM)
@@ -49,16 +49,20 @@ class Models:
 
         return top_model
 
-    @property
-    def __build_nasnetL(self):
-        model = NASNetLarge(weights='imagenet', include_top=False, input_shape=(IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNEL))
         
+
+    @property
+    def __build_inceptionresnetv2(self):
+        model = InceptionResNetV2(weights='imagenet', include_top=False, input_shape=(IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNEL))
+        for layer in model.layers[:10]:
+            layer.trainable = False
         top_model = Sequential()
         top_model.add(model)
+        top_model.add(GlobalAveragePooling2D())
         top_model.add(Flatten())
         top_model.add(Dense(256, activation='relu'))
         top_model.add(Dropout(0.3))
-        top_model.add(Dense(CLASS_NUM, activation='sigmoid'))
+        top_model.add(Dense(42, activation='softmax'))
 
         return top_model
 
@@ -67,8 +71,8 @@ class Models:
             model = self.__build_vgg16
         elif self.model_type == 'xception':
             model = self.__build_xception
-        elif self.model_type == 'nasnetL':
-            model = self.__build_nasnetL
+        elif self.model_type == 'inceptionresnetv2':
+            model = self.__build_inceptionresnetv2
         else:
             raise Exception('Invalid model name')
 
@@ -81,8 +85,8 @@ class Models:
             optimizer=optimizers.Adam(lr=LEARNING_RATE),
             metrics=['accuracy'])
 
-        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2)
-        checkpoint_filepath = '/content/drive/My Drive/sopikodelig/inceptionv3-{epoch:02d}-{val_accuracy:.4f}.hdf5'
+        # reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2)
+        checkpoint_filepath = '/content/drive/My Drive/sopikodelig/inceptionresnetv2-{epoch:02d}-{val_accuracy:.4f}.hdf5'
         model_checkpoint_callback = ModelCheckpoint(
             filepath=checkpoint_filepath,
             save_weights_only=True,
@@ -95,7 +99,7 @@ class Models:
             steps_per_epoch=self.train_set.n // BATCH_SIZE,
             epochs=EPOCH_NUM,
             workers=6,
-            callbacks=[reduce_lr, model_checkpoint_callback],
+            callbacks=[model_checkpoint_callback],
             validation_data=self.validation_set,
             validation_steps=self.validation_set.n // BATCH_SIZE)
 
